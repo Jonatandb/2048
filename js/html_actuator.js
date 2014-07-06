645711ef4228c81c1a5b205a4ef54cd86d06e3dc
Jonatandb <jonatandb@gmail.com>
2014-07-06 16:30:03 -0300

    Traducciones faltantes
    Agregado de imagen de cuadro final.
    Traducción de mensajes de victoria y juego terminado.

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -146,7 +146,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Ganaste!" : "Sin movimientos!";
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
